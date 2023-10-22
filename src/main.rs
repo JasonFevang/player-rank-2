c4789e966129b2a31cb69d8fedaea60b16db7371
Jason Fevang <jasonfevang@gmail.com>
2023-10-22 13:43:09 -0700

    validate argument file existence

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,17 @@
 use clap::Parser;
 use std::fmt;
+use std::fs;
+use std::io;
 
 // The triple-slash comments can be read by Rust's procedural macros and are used to populate the help message. That's  crazy
-/// Search for a pattern in a file and display the lines that contain it.
+/// This command is used to determine relative player rankings through a series of questions comparing two players' abilities. Provide a list of player's names to begin
 #[derive(Parser)]
 struct Cli {
-    /// The path to the file to read
+    /// CSV with a list of players and information about them
     player_file: std::path::PathBuf,
+    /// CSV with a list of questions with the provided comparisions. May or may not already exist
     question_file: std::path::PathBuf,
+    /// CSV output file with relative rankings for each player
     output_file: std::path::PathBuf,
 }
 
@@ -21,7 +25,29 @@ impl fmt::Debug for Cli {
     }
 }
 
-fn main() {
-    let _args = Cli::parse();
-    println!("{:?}", _args)
+fn file_exists(file_path: &std::path::PathBuf) -> io::Result<()> {
+    let metadata = fs::metadata(file_path)?;
+    if !metadata.is_file(){
+        return Err(io::Error::new(
+            io::ErrorKind::InvalidInput,
+            "The path is not a file",
+        ));
+    }
+    Ok(())
+}
+
+fn validate_arguments(args: &Cli) -> io::Result<()> {
+    file_exists(&args.player_file)?;
+    file_exists(&args.question_file)?;
+    file_exists(&args.output_file)?;
+    Ok(())
+}
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    // Parse arguments
+    let args = Cli::parse();
+    println!("{:?}", args);
+
+    validate_arguments(&args)?;
+    Ok(())
 }

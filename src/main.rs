074d4eb5cab65dd373ab96be3439c9b66dd486da
Jason Fevang <jasonfevang@gmail.com>
2023-11-02 12:05:48 -0700

    Add player_rank_lib structs and stubbed out file<->struct functions

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,11 @@
 use anyhow::{Context, Result};
 use clap::Parser;
+use log::trace;
 use std::fmt;
 use std::fs;
 use std::io;
-use log::trace;
+
+mod player_rank_lib;
 
 // The triple-slash comments can be read by Rust's procedural macros and are used to populate the help message. That's  crazy
 /// This command is used to determine relative player rankings through a series of questions comparing two players' abilities. Provide a list of player's names to begin
@@ -49,14 +51,70 @@ fn validate_arguments(args: &Cli) -> Result<()> {
     Ok(())
 }
 
+fn parse_player_file(_player_file: &std::path::PathBuf) -> player_rank_lib::Players {
+    let mut players = player_rank_lib::Players::new();
+    players.players.push(player_rank_lib::Player {
+        name: String::from("Jason"),
+        goalie: true,
+        avail1: true,
+        avail2: true,
+    });
+    players
+}
+
+fn parse_question_file(_question_file: &std::path::PathBuf) -> player_rank_lib::Questions {
+    let mut questions = player_rank_lib::Questions::new();
+
+    // Add a dummy question
+    questions.questions.push(player_rank_lib::AnsweredQuestion {
+        question: player_rank_lib::Question {
+            player1: String::from("Jason"),
+            pos1: player_rank_lib::Position::Atk,
+            player2: String::from("Max"),
+            pos2: player_rank_lib::Position::Def,
+        },
+        response: 1.2,
+    });
+    questions
+}
+
+fn write_question_file(
+    _file: &std::path::PathBuf,
+    _questions: &player_rank_lib::Questions,
+) -> Result<()> {
+    Ok(())
+}
+
+fn write_rank_file(_file: &std::path::PathBuf, _ranks: &player_rank_lib::Ranks) -> Result<()> {
+    Ok(())
+}
+
+fn run_ranking() -> Result<player_rank_lib::Ranks> {
+    Ok(player_rank_lib::Ranks::new())
+}
+
 fn main() -> Result<()> {
     env_logger::init();
-    // Parse arguments
 
     trace!("Parsing arguments");
     let args = Cli::parse();
-    println!("{:?}", args);
 
     validate_arguments(&args)?;
+    println!("{:?}", args);
+
+    // Convert files into their respective structs
+    let players = parse_player_file(&args.player_file);
+    let questions = parse_question_file(&args.player_file);
+
+    // Print parsed players
+    for player in &players.players {
+        println!("{:?}", player);
+    }
+
+    let ranks = run_ranking()?;
+
+    // Write the outputs back to file
+    write_question_file(&args.question_file, &questions)?;
+    write_rank_file(&args.output_file, &ranks)?;
     Ok(())
 }
---
+++ src/player_rank_lib.rs
@@ -0,0 +1,80 @@
+use std::fmt;
+
+//// Structures ////
+pub struct Player {
+    pub name: String,
+    pub goalie: bool,
+    pub avail1: bool,
+    pub avail2: bool,
+}
+
+impl fmt::Debug for Player {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        f.debug_struct("Player")
+            .field("name", &self.name)
+            .field("goalie", &self.goalie)
+            .field("avail1", &self.avail1)
+            .field("avail2", &self.avail2)
+            .finish()
+    }
+}
+
+/// Player Rank Interface: Input, player list
+pub struct Players {
+    pub players: Vec<Player>,
+}
+
+impl Players {
+    pub fn new() -> Self {
+        Players {
+            players: Vec::new(),
+        }
+    }
+}
+
+pub enum Position {
+    Atk,
+    Def,
+    Goalie,
+}
+
+pub struct Question {
+    pub player1: String,
+    pub pos1: Position,
+    pub player2: String,
+    pub pos2: Position,
+}
+
+pub struct AnsweredQuestion {
+    pub question: Question,
+    pub response: f64,
+}
+
+/// Player Rank Interface: Input/output, questions list
+pub struct Questions {
+    pub questions: Vec<AnsweredQuestion>,
+}
+
+impl Questions {
+    pub fn new() -> Self {
+        Questions {
+            questions: Vec::new(),
+        }
+    }
+}
+
+/// Player Rank Interface: Output, rank list
+pub struct Rank {
+    pub name: String,
+    pub rank: f64,
+}
+
+pub struct Ranks {
+    pub ranks: Vec<Rank>,
+}
+
+impl Ranks {
+    pub fn new() -> Self {
+        Ranks { ranks: Vec::new() }
+    }
+}
